6948b8eed9daae1319c0789a93f347a99ee981aa
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-02-11 21:19:40 +0100

    Second commit

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
 import pygame
 import random
 
-# creating the data structure for pieces
-# setting up global vars
-# functions
-# - create_grid
-# - draw_grid
-# - draw_window
-# - rotating shape in main
-# - setting up the main
-from pygame import surface
 
-"""
-10 x 20 square grid
-shapes: S, Z, I, O, J, L, T
-represented in order by 0 - 6
-"""
 
 pygame.font.init()
 
 # GLOBALS VARS
 s_width = 800
 s_height = 700
-play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 20 height per block
+play_width = 300
+play_height = 600
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
 
 # SHAPE FORMATS
-
 S = [['.....',
      '......',
      '..00..',
